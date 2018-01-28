f7d94c20863846f2dcc814f6f70ffe1f2a1d494c
Daniel <esquinazi.dani@gmail.com>
2018-01-27 21:07:13 -0300

    Implement CRUD on Todo items
    Create endpoints in order to make CRUD operations over todo items.

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,14 @@
-import mongoose from ('mongoose');
+import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const TodoSchema = Schema({
   name: { type: String, required: true },
   description: String,
   completed: { type: Boolean, default: false },
-  dueDate: { type: Date, default: Date.now() }
+  dueDate: { type: Date, default: Date.now() },
 });
 
 const TodoModel = mongoose.model('Todo', TodoSchema);
 
-export{
-  TodoModel
-}
+export default TodoModel;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
+import mongoose from 'mongoose';
 import express from 'express';
-import { port, host } from '../config';
+import bodyParser from 'body-parser';
+import { port, host, db } from '../config';
+import api from '../routes';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api', api);
 
-app.listen(port, () => {
-  console.log(`Server running on http://${host}:${port}`);
+mongoose.connect(db, {});
+const dataBaseConnection = mongoose.connection;
+dataBaseConnection.on('error', console.error.bind(console, 'connection error:'));
+dataBaseConnection.on('open', () => {
+  app.listen(port, () => {
+    console.log(`Server running on http://${host}:${port}`);
+  });
 });
