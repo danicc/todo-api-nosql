271a7e6c0e2c1e85f079c2c36fcf977a0c2e5986
Esquinazi Daniel <desquinazi@black-asus.home>
2018-08-11 11:43:48 -0300

    Remove auth for /todos endpoint

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import TodoController from '../controllers/todo';
 import checkTodoId from '../middleware/check-todo-id';
-import auth from '../middleware/auth';
+// import auth from '../middleware/auth';
 
 const todoRouter = express.Router();
 
-todoRouter.use(auth);
+// todoRouter.use(auth);
 
 todoRouter.param('id', checkTodoId);
 
 todoRouter.get('/', TodoController.GetTodos);
 todoRouter.get('/:id', TodoController.GetTodo);
-todoRouter.post('/', auth, TodoController.PostTodo);
-todoRouter.put('/:id', auth, TodoController.PutTodo);
-todoRouter.delete('/:id', auth, TodoController.DeleteTodo);
+// todoRouter.post('/', auth, TodoController.PostTodo);
+// todoRouter.put('/:id', auth, TodoController.PutTodo);
+// todoRouter.delete('/:id', auth, TodoController.DeleteTodo);
 
 export default todoRouter;
 
